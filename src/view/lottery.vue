2c5db9871a7b9a587720511ee8d5c0b1c8719539
chenchen <chenchen@p1.com>
2019-11-19 09:40:50 +0800

    add calendar

--- src/view/lottery.vue
+++ src/view/lottery.vue
@@ -1,12 +1,34 @@
 <template>
-    <div>彩票</div>
+  <div>
+    <v-calendar
+      class="pa-8"
+      locale="zhHant"
+      color="primary"
+      :events="events"
+      :event-color="getEventColor"
+    ></v-calendar>
+  </div>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        
-    })
-}
+  data: () => ({
+    events: [
+      {
+        name: "中奖啦",
+        details: "这次真中奖啦",
+        start: "2019-11-08",
+        color: "blue"
+      }
+    ]
+  }),
+  methods: {
+    getEventColor(event) {
+      return event.color;
+    }
+  }
+};
 </script>
 
+<style>
+</style>
